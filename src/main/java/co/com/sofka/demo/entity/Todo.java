4b8e84fa2c694c5cf5f857822d0415c143d6a486
DanielAristy <danielaristy22@gmail.com>
2021-06-01 21:24:15 -0500

    Creando la entidad, en la que se va a tomar para crear las tablas en la base de datos

--- src/main/java/co/com/sofka/demo/entity/Todo.java
+++ src/main/java/co/com/sofka/demo/entity/Todo.java
@@ -1,5 +1,41 @@
 package co.com.sofka.demo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Todo {
 
+    //Identificando el primaryKey de la clase
+    @Id
+    //Generar el valor automaticamente autoincrementado
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private boolean esCompletado;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isEsCompletado() {
+        return esCompletado;
+    }
+
+    public void setEsCompletado(boolean esCompletado) {
+        this.esCompletado = esCompletado;
+    }
 }
